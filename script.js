1b44e98bce0f7322ad80dda45258c4629f8c9233
connellyguy <joshua@joshc.net>
2020-09-14 12:05:17 -0400

    Version 2 Sync

--- script.js
+++ script.js
@@ -1,51 +1,92 @@
-function pendoFunction(apiKey, visitorId, _lightningOption){
-
-  if(_lightningOption) {
-
-  (function(apiKey){
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
-        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-        y=e.createElement(n);y.async=!0;y.src="https://pendo-io-static.storage.googleapis.com/agent/releases/2.59.1/pendo.lightning.min.js";
-        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-    })(apiKey);
+function pendoFunction(apiKey, visitorId, _lightningOption) {
+    if (_lightningOption) {
+        (function (apiKey) {
+            (function (p, e, n, d, o) {
+                var v, w, x, y, z;
+                o = p[d] = p[d] || {};
+                o._q = [];
+                v = [
+                    "initialize",
+                    "identify",
+                    "updateOptions",
+                    "pageLoad",
+                    "track",
+                ];
+                for (w = 0, x = v.length; w < x; ++w)
+                    (function (m) {
+                        o[m] =
+                            o[m] ||
+                            function () {
+                                o._q[m === v[0] ? "unshift" : "push"](
+                                    [m].concat([].slice.call(arguments, 0))
+                                );
+                            };
+                    })(v[w]);
+                y = e.createElement(n);
+                y.async = !0;
+                y.src =
+                    "https://pendo-io-static.storage.googleapis.com/agent/releases/2.65.1/pendo.lightning.min.js";
+                z = e.getElementsByTagName(n)[0];
+                z.parentNode.insertBefore(y, z);
+            })(window, document, "script", "pendo");
+        })(apiKey);
         // Call this whenever information about your visitors becomes available
-    pendo.initialize({ apiKey: apiKey, visitor: {id: visitorId}});
+        pendo.initialize({
+            apiKey: apiKey,
+            visitor: { id: visitorId },
+            account: {
+                id: "test-account",
+            },
+        });
 
+    } else {
+        (function (p, e, n, d, o) {
+            var v, w, x, y, z;
+            o = p[d] = p[d] || {};
+            o._q = [];
+            v = ["initialize", "identify", "updateOptions", "pageLoad"];
+            for (w = 0, x = v.length; w < x; ++w)
+                (function (m) {
+                    o[m] =
+                        o[m] ||
+                        function () {
+                            o._q[m === v[0] ? "unshift" : "push"](
+                                [m].concat([].slice.call(arguments, 0))
+                            );
+                        };
+                })(v[w]);
+            y = e.createElement(n);
+            y.async = !0;
+            y.src = "https://cdn.pendo.io/agent/static/" + apiKey + "/pendo.js";
+            z = e.getElementsByTagName(n)[0];
+            z.parentNode.insertBefore(y, z);
+        })(window, document, "script", "pendo");
 
-  console.log(_lightningOption);
+        pendo.initialize({
+            visitor: {
+                id: visitorId,
+            },
 
-  }
-
-  else {
-
-  (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-    v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
-    o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-    y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
-    z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-
-  pendo.initialize({
-    visitor: {
-        id:              visitorId   
-    },
-
-    account: {
-        id:           'test-account' 
+            account: {
+                id: "test-account",
+            },
+        });
     }
-  });
+}
 
-  }
-
-
-
-};
-
-window.addEventListener("message", function(event) {
-    if(event.data.type === 'pendoKey') {
-        window._pendoKeyValue = event.data.value[0];
-        window._pendoVisitorIdValue = event.data.value[1];
-        window._lightningOption = event.data.value[2];
-        pendoFunction(_pendoKeyValue, _pendoVisitorIdValue, _lightningOption); 
-    };
-}, false);
+window.addEventListener(
+    "message",
+    function (event) {
+        if (event.data.type === "pendoInjectorKey") {
+            var _pendoKeyValue = event.data.keyValue;
+            var _pendoVisitorIdValue = event.data.idValue;
+            var _lightningOption = event.data.lightningValue;
+            pendoFunction(
+                _pendoKeyValue,
+                _pendoVisitorIdValue,
+                _lightningOption
+            );
+        }
+    },
+    false
+);
